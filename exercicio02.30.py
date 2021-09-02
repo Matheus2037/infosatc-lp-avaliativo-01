64ca3e77ec27a2aca641e1914224739984e012ce
Matheus2037 <matheusdasilvagastaldi@gmail.com>
2021-09-02 14:23:19 -0300

    exercicio02.30
    pensou q ia ser hehe dnovo kkkk :anguished:

---
+++ exercicio02.30.py
@@ -0,0 +1,7 @@
+R = float(input("Digite a quantidade de reais: "))
+
+D = float(input("Digite o valor do dolar: "))
+
+C = R / D
+
+print("A quantia que pode ser convertida para dolares é de: {:.2f} ".format(C))
